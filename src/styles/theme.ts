0aeba44862b6edf8efda9850610d6ae339b6890a
Hyunggeun Jo <geunee92@gmail.com>
2025-06-11 16:28:37 +0900

    chore: emotion 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  background: "#ffffff",
+  text: "#000000",
+};
+
+export const darkTheme = {
+  background: "#121212",
+  text: "#ffffff",
+};
+
+export type ThemeType = typeof lightTheme;
